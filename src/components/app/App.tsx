d887ebbc6ef331f3fffd04f0aaeb5bbd83b1ce3f
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-03-23 20:55:33 +0100

    fixed

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Structure from '../layout/structure';
-import { Container, Box, Typography, Button } from '@material-ui/core';
+import { Container, Typography, Button, Box } from '@material-ui/core';
 import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
 
 
@@ -9,8 +9,8 @@ import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
 function App() {
   return (
     <ScopedCssBaseline>
-      <Container maxWidth="sm">
-        <Box my={4}>
+      <Container fixed>
+        <Box my={4} >
           <Typography variant="h2" component="h1" gutterBottom>
             Create React App v4-beta example
         </Typography>

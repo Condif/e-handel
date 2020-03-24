5fce57e1d2a846bc469adab8b06a32024558f368
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-03-24 09:49:40 +0100

    trial Base

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
-import './App.css';
-import Structure from '../layout/structure';
-import { Container, Typography, Button, Box } from '@material-ui/core';
-import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
+import React from "react";
+import "./App.css";
+import Footer from "../footer/footer";
+import CardMaker from "../cardMaker/cardMaker";
 
-
+import { Container, Typography, Button, Box } from "@material-ui/core";
+import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
 
 function App() {
-  return (
-    <ScopedCssBaseline>
-      <Container fixed>
-        <Box my={4} >
-          <Typography variant="h2" component="h1" gutterBottom>
-            Create React App v4-beta example
-        </Typography>
-        </Box>
-        <Button variant="contained" color="primary">
-          Hello World
-     </Button>
-      </Container>
-    </ScopedCssBaseline>
+	const cards = [
+		{
+			productName: "one",
+			imgSrc:
+				"https://images.unsplash.com/photo-1556038024-327f281f00b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
+			price: "14:-",
+			description:
+				"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto excepturi dolorum quia harum..."
+		},
+		{
+			productName: "two",
+			imgSrc:
+				"https://images.unsplash.com/photo-1572933014070-3aec28ebba4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=897&q=80",
+			price: "999:-",
+			description:
+				"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto excepturi dolorum quia harum..."
+		},
+		{
+			productName: "three",
+			imgSrc:
+				"https://images.unsplash.com/photo-1583189774117-0d355da61fac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+			price: "420:-",
+			description:
+				"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto excepturi dolorum quia harum..."
+		},
+	];
 
+	return (
+		<ScopedCssBaseline>
+			<Container fixed>
+				{cards.map((card: any) => (
+          <CardMaker product={card} ></CardMaker>
+				))}
+			</Container>
 
-  )
+			<Footer />
+		</ScopedCssBaseline>
+	);
 }
 
 export default App;
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import HomeIcon from '@material-ui/icons/Home';
+
+const useStyles = makeStyles({
+  root: {
+    position:"fixed",
+    bottom:0,
+
+    width: "100%",
+
+    background:"#e7e7e7"
+  }
+});
+
+export default function Footer() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setValue(newValue);
+  };
+  console.log(value);
+  
+
+  return (
+    <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+      <BottomNavigationAction label="Home" value="home" icon={<HomeIcon  />} />
+      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Search" value="search" icon={<ShoppingCartIcon />} />
+    </BottomNavigation>
+  );
+}

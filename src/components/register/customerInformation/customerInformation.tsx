b75ac8fb6526eb5f543f52770f75c8d4ca84b356
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-25 22:25:31 +0100

    removed unused code

--- src/components/register/customerInformation/customerInformation.tsx
+++ src/components/register/customerInformation/customerInformation.tsx
@@ -23,80 +23,78 @@ export default function CustomerInformation() {
     const classes = useStyles();
 
     return (
-        // <form className={classes.root} noValidate autoComplete="on">
-            <div>
-                <Typography variant="h5" className={classes.title}>
-                    Your information
-                </Typography>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6} md={6}>
-                        <FormControl fullWidth>
-                            <TextField 
-                            required 
-                            id="standard-required" 
-                            label="First name" 
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6}>
-                        <FormControl fullWidth>
-                            <TextField
-                              required
-                              id="standard-required"
-                              label="Last name"
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid item sm={12} xs={12}>
-                        <FormControl fullWidth>
-                            <TextField
-                              required
-                              id="outlined-number"
-                              label="Mobile number"
-                              type="number"
-                            //   InputLabelProps={{
-                            //     shrink: true,
-                            //   }}
-                              variant="outlined"
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid item sm={12} xs={12}>
-                        <FormControl fullWidth>
-                            <TextField
-                              required
-                              id="outlined-required"
-                              label="Address"
-                              variant="outlined"
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6}>
-                        <FormControl fullWidth>
-                            <TextField
-                              required
-                              id="outlined-number"
-                              label="Postal code"
-                              type="number"
-                            //   InputLabelProps={{
-                            //     shrink: true,
-                            //   }}
-                              variant="outlined"
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6}>
-                        <FormControl fullWidth>
-                            <TextField
-                              required
-                              id="outlined-required"
-                              label="City"
-                              variant="outlined"
-                            />
-                        </FormControl>
-                    </Grid>
+        <div>
+            <Typography variant="h5" className={classes.title}>
+                Your information
+            </Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6} md={6}>
+                    <FormControl fullWidth>
+                        <TextField 
+                        required 
+                        id="standard-required" 
+                        label="First name" 
+                        />
+                    </FormControl>
                 </Grid>
-            </div>
-        // </form>
+                <Grid item xs={12} sm={6} md={6}>
+                    <FormControl fullWidth>
+                        <TextField
+                            required
+                            id="standard-required"
+                            label="Last name"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item sm={12} xs={12}>
+                    <FormControl fullWidth>
+                        <TextField
+                            required
+                            id="outlined-number"
+                            label="Mobile number"
+                            type="number"
+                        //   InputLabelProps={{
+                        //     shrink: true,
+                        //   }}
+                            variant="outlined"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item sm={12} xs={12}>
+                    <FormControl fullWidth>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Address"
+                            variant="outlined"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                    <FormControl fullWidth>
+                        <TextField
+                            required
+                            id="outlined-number"
+                            label="Postal code"
+                            type="number"
+                        //   InputLabelProps={{
+                        //     shrink: true,
+                        //   }}
+                            variant="outlined"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                    <FormControl fullWidth>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="City"
+                            variant="outlined"
+                        />
+                    </FormControl>
+                </Grid>
+            </Grid>
+        </div>
     )
 }

0a3f61236a60db54a07d3afad8686894e2693422
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-26 14:11:32 +0100

    rebuilt list generation

--- src/components/register/deliveryOptions/deliveryOptions.tsx
+++ src/components/register/deliveryOptions/deliveryOptions.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Typography, List, ListItem, ListItemText, ListItemIcon, Radio, Collapse } from '@material-ui/core'
+import { Typography, List } from '@material-ui/core'
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { DeliveryTypes, Delivery } from './deliveryAPI'
+import RegisterListItem from '../registerListItem/registerListItem';
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
@@ -25,10 +26,11 @@ export default function DeliveryOptions() {
     const [selectedIndex, setSelectedIndex] = React.useState(0);
 
     const handleListItemClick = (
-      event: React.MouseEvent<HTMLDivElement, MouseEvent> | React.ChangeEvent<HTMLInputElement>,
-      index: number,
+    //   event: React.MouseEvent<HTMLDivElement, MouseEvent> | React.ChangeEvent<HTMLInputElement>,
+      index: number | string,
     ) => {
-        setSelectedIndex(index);
+        (typeof index === 'number') ? setSelectedIndex(index) : console.log(index);
+        
     };
 
     return (
@@ -39,34 +41,16 @@ export default function DeliveryOptions() {
             <div className={classes.demo}>
                 <List component="nav" aria-label="Delivery Methods">
                     {DeliveryTypes.map((element: Delivery, index) =>
-                    <>
-                        <ListItem
-                        button
-                        selected={selectedIndex === index}
-                        onClick={event => handleListItemClick(event, index)}
-                        >
-                            <Radio
-                                checked={selectedIndex === index}
-                                onChange={event => handleListItemClick(event, index)}
-                                color="default"
-                                name="OptionOne"
-                            />
-                            <ListItemText primary={element.name}  secondary={(selectedIndex !== index) ? element.deliveryTime : null} />
-                            <ListItemText
-                             style={{
-                                 textAlign: "right"
-                             }}
-                             primary={`${element.price}:-`}
-                             />
-                        </ListItem>
-                        <Collapse in={(selectedIndex === index)} timeout="auto" unmountOnExit>
-                            <List component="div" disablePadding>
-                              <ListItem selected className={classes.nested}>
-                                <ListItemText primary={`${element.desc} within ${element.deliveryTime}.`} />
-                              </ListItem>
-                            </List>
-                          </Collapse>
-                        </>
+                        <RegisterListItem 
+                            key={element.name}
+                            selectedIndex={selectedIndex}
+                            identifier={index}
+                            handleListItemClick={handleListItemClick}
+                            name={element.name}
+                            desc={element.desc}
+                            delTime={element.deliveryTime}
+                            price={element.price}
+                        />
                     )}
                 </List>
             </div>

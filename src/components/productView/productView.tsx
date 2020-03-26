5f32201217e9dcaa960aa3e2aa513f9f355bc017
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-03-26 10:52:36 +0100

    added product view

---
+++ src/components/productView/productView.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { RouteChildrenProps } from "react-router";
+import { ProductList } from "../mockProducts/productsAPI";
+import CardFactory from "../cardFactory/cardFactory";
+import {
+	CssBaseline,
+	Container,
+	Typography,
+	Grid,
+	makeStyles,
+	Theme,
+	createStyles
+} from "@material-ui/core";
+
+interface Props extends RouteChildrenProps<{ serial: string }> {}
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		wrapper: {
+			minHeight: "100vh",
+			height: "100%"
+		}
+	})
+);
+
+const ProductView: React.FC<Props> = ({ match }: any) => {
+	const classes = useStyles();
+
+	const serialNumber = parseInt(match.params.serial);
+
+	const product = ProductList[serialNumber - 1] || undefined;
+
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Container maxWidth="md" className={classes.wrapper}></Container>
+			<CardFactory product={product} view="fullpage" />
+		</React.Fragment>
+	);
+};
+
+export default ProductView;

0da498acf7d386b0eced1468d200a098d927819a
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-28 11:05:07 +0100

    created placeholder delivery type

--- src/components/register/deliveryOptions/deliveryAPI.tsx
+++ src/components/register/deliveryOptions/deliveryAPI.tsx
@@ -1,24 +1,38 @@
-export interface Delivery {
-    name: string,
-    desc: string,
+import { RegisterAPI } from "../registerAPI"
+
+export interface DeliveryOption extends RegisterAPI {
+    type: 'del',
+    // name: string,
+    // desc: string,
     deliveryTime: string,
-    price: number,
+    // price: number,
 }
 
-export const DeliveryTypes: Delivery[] = [
+export const baseDelivery: DeliveryOption = {
+    type: 'del',
+    name: 'Parcel',
+    desc: 'Deliver to your postal office',
+    deliveryTime: '2-3 work days',
+    price: 99,
+}
+
+export const DeliveryTypes: DeliveryOption[] = [
     {
+        type: 'del',
         name: 'Parcel',
         desc: 'Deliver to your postal office',
         deliveryTime: '2-3 work days',
         price: 99,
     },
     {
+        type: 'del',
         name: 'Home delivery',
         desc: 'Deliver to your front door',
         deliveryTime: '2-3 work days',
         price: 199,
     },
     {
+        type: 'del',
         name: 'Express',
         desc: 'Deliver to your front door',
         deliveryTime: '1 work day',

3e08a06129a7965487eda264d5beca1dc64864e1
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-28 11:05:47 +0100

    created base payment API

--- src/components/register/paymentOptions/paymentAPI.tsx
+++ src/components/register/paymentOptions/paymentAPI.tsx
@@ -1,34 +1,56 @@
-export interface PaymentOption {
-    identifier: string,
-    name: string,
-    desc: string,
-    fee: string | number
+import { RegisterAPI } from "../registerAPI"
+
+export interface PaymentOption extends RegisterAPI {
+    type: 'pay',
+    // name: string,
+    // desc: string,
+    // price: string | number
+    options: any[] | false
+}
+
+export const basePayment: PaymentOption = {
+    type: 'pay',
+    name:'none',
+    desc: 'none',
+    price: 'none',
+    options: false
 }
 
 export const PaymentTypes: PaymentOption[] = [
     {
-        identifier: 'Card',
+        type: 'pay',
         name: 'Card',
         desc: 'Pay immediately on delivery',
-        fee: 'free'
+        price: 'free',
+        options: false
     },
     {
-        identifier: 'SWISH',
+        type: 'pay',
         name: 'Swish',
         desc: 'Pay by Swish immediately on purchase',
-        fee: 'free'
+        price: 'free',
+        options: false
     },
     {
-        identifier: 'PAYPL',
+        type: 'pay',
         name: 'Paypal',
         desc: 'Pay by Paypal immediately on purchase',
-        fee: 'free'
+        price: 'free',
+        options: false
     },
     {
-        identifier: 'KLARN',
+        type: 'pay',
         name: 'Klarna Invoice',
         desc: 'Pay by invoice after purchase',
-        fee: 'fee'
+        price: 'fee',
+        options: [{
+            value: 'KLA14',
+            label: '14 day invoice'
+        },
+        {
+            value: 'KLA3M',
+            label: '3 months invoice'
+        }]
     },
     
 ]

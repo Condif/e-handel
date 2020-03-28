8a686a1054950648853a1a8d17676e337b097fc6
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-28 11:06:54 +0100

    started working on chosen payment type

--- src/components/register/paymentOptions/chosenPayment/chosenPayment.tsx
+++ src/components/register/paymentOptions/chosenPayment/chosenPayment.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { PaymentTypes, PaymentOption } from '../paymentAPI'
-import { Container, makeStyles, Theme, Typography } from '@material-ui/core'
+import { Container, makeStyles, Theme, Typography, TextField, MenuItem, FormControl } from '@material-ui/core'
 import StoreRoundedIcon from '@material-ui/icons/StoreRounded';
 
 interface Props {
-    method: string
+    identifier: PaymentOption
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
     blankTitle: {
         textAlign: 'center',
-        margin: theme.spacing(1,1)
+        margin: theme.spacing(2, 1)
     },
-    wrapper: {
-        padding: theme.spacing(0,1,2),
+    noSelection: {
+        padding: theme.spacing(0, 1, 2),
         height: '100%',
         border: '2px dashed #EBEBEB',
         borderRadius: '1rem',
@@ -21,27 +21,84 @@ const useStyles = makeStyles((theme: Theme) => ({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    wrapper: {
+        padding: theme.spacing(0, 1, 2),
+        height: '100%',
+        border: '2px solid #EBEBEB',
+        borderRadius: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start'
+    },
+    textField: {
+        margin: theme.spacing(1, 1)
     }
 }))
 
-const blankPage = (classes: Record<"blankTitle" | "wrapper", string>) => {
+const blankPage = (classes: Record<"blankTitle" | "noSelection", string>) => {
     return (
-        <Container className={classes.wrapper}>
-             <Typography variant="h5" className={classes.blankTitle}>
+        <Container className={classes.noSelection}>
+            <Typography variant="h5" className={classes.blankTitle}>
                 Choose a payment method
             </Typography>
-            <StoreRoundedIcon fontSize="large"/>
+            <StoreRoundedIcon fontSize="large" />
         </Container>
+    )
+}
+
+const generatePurchaseOptions = (classes: any, props: Props, type: string, handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void) => {
+    console.log(props.identifier);
+
+    return (
+        <Container className={classes.wrapper}>
+            <Typography variant="h5" className={classes.blankTitle}>
+                {props.identifier.name}
+            </Typography>
+            {generateOptionalDropDown(classes, props, type, handleChange)}
+            
+        </Container>
+    )
+}
+
+const generateOptionalDropDown = (classes: any, props: Props, type: string, handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void) => {
+    return (
+        (props.identifier.options) ? 
+        <FormControl fullWidth>
+            <TextField
+                className={classes.textField}
+                select
+                label={`${props.identifier.name} type:`}
+                value={type}
+                onChange={handleChange}
+                helperText={`Please select type`}
+            >
+                {props.identifier.options.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+        </FormControl>
+        : null
     )
 }
 
 export default function ChosenPayment(props: Props) {
     const classes = useStyles();
+    const [type, setType] = React.useState('VISA');
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('change');
+
+        setType(event.target.value);
+    };
+
     return (
         <>
-            {(props.method === 'none') 
-                ?  blankPage(classes)
-                : null
+            {(props.identifier.name === 'none')
+                ? blankPage(classes)
+                : generatePurchaseOptions(classes, props, type, handleChange)
             }
         </>
     )

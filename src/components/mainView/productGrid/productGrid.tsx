8a63ef4166d39e9e5ef25f39eeb7e9a99212b109
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-03-29 14:43:38 +0200

    added modal

--- src/components/mainView/productGrid/productGrid.tsx
+++ src/components/mainView/productGrid/productGrid.tsx
@@ -6,13 +6,21 @@ import {
 	Theme,
 	createStyles,
 	Container,
-	Typography
+	Typography,
+	Modal,
+	Paper,
+	TextField,
+	FormControl,
+	InputLabel,
+	Input,
+	FormHelperText
 } from "@material-ui/core";
 import CardFactory from "../../productFactory/productFactory";
 import { ProductContext } from "../../../contexts/productContext";
 import { ProductList } from "../../productsAPI/productsAPI";
 import { CSSProperties } from "@material-ui/core/styles/withStyles";
 import { Product } from "../../../interfaces&types/interfaces";
+import { open } from "fs";
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -24,6 +32,20 @@ const useStyles = makeStyles((theme: Theme) =>
 			margin: "1rem 0",
 			borderBottom: "2px solid #97ADA188",
 			color: "#4B6155"
+		},
+		modalWrapper: {
+			width: "100vw",
+			height: "100vh",
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center"
+		},
+		modalContent: {
+			width: "50%",
+			height: "60%",
+
+			margin: "5rem",
+			padding: "2rem"
 		}
 	})
 );
@@ -41,24 +63,57 @@ interface Props {
 function ProductGrid(props: Props) {
 	const classes = useStyles();
 
+	// MODAL
+	const [open, setOpen] = React.useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	return (
 		<ProductContext.Consumer>
-			{({products, removeLastItem, addNewItem}) => (
+			{({ products, removeLastItem, addNewItem }) => (
 				<Container maxWidth="lg" disableGutters={false}>
 					<Typography variant="h2" className={classes.header}>
 						Products
 					</Typography>
 
 					<div style={btnWrapper}>
-						<button onClick={removeLastItem}>
-							-
-						</button>
+						<button onClick={removeLastItem}>-</button>
+						<button onClick={handleOpen}>+</button>
 					</div>
-					<div style={btnWrapper}>
-						<button onClick={addNewItem}>
-							+
-						</button>
-					</div>
+
+					<Modal
+						aria-labelledby="simple-modal-title"
+						aria-describedby="simple-modal-description"
+						open={open}
+						onClose={handleClose}>
+						<div className={classes.modalWrapper}>
+							<Paper className={classes.modalContent}>
+								<Typography variant="h4">add new item</Typography>
+								<form className={classes.root} noValidate autoComplete="off">
+									<div>
+										<TextField
+											id="name"
+											label="name"
+											placeholder="add name"
+											helperText="name of the new product"
+										/>
+										<TextField
+											id="price"
+											label="price"
+											placeholder="add price"
+											helperText="price of the new product"
+										/>
+									</div>
+								</form>
+							</Paper>
+						</div>
+					</Modal>
 
 					<Grid
 						container

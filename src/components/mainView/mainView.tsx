ddd03fa45187e142529391ad2215e3186d782da8
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-03-30 11:26:15 +0200

    moved functionality from main

--- src/components/mainView/mainView.tsx
+++ src/components/mainView/mainView.tsx
@@ -11,41 +11,13 @@ interface State {
 	products: Product[];
 }
 class MainView extends React.Component<Props, State> {
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			products: ProductList
-		};
-	}
-	handleAddNewProduct =() => {
-		console.log("hej");
-		
-	}
-
 	render() {
-		const newProduct: Product = {
-			name: "munk",
-			serial: this.state.products.length + 1,
-			img:
-				"https://images.unsplash.com/photo-1584727638096-042c45049ebe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=712&q=80",
-			price: 10000,
-			desc:
-				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ea veritatis hic, vero fugit...",
-			info: {
-				sizes: ["xs", "sm", "md", "lg", "xl"],
-				colors: ["red", "green", "blue", "orange"]
-			}
-		};
-
 		return (
 			<Switch>
 				<Route exact path="/">
-					<ProductGrid
-						addNewProduct={this.handleAddNewProduct}
-						products={this.state.products}
-					/>
+					<ProductGrid />
 				</Route>
-				
+
 				<Route path="/productview/:serial" exact component={ProductView} />
 
 				<Route>something went wrong</Route>

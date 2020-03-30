50abc74f850f033ccfdbc5346ea8f659be138170
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-03-31 00:13:08 +0200

    removefromcart is back

--- src/components/contextButton/contextButton.tsx
+++ src/components/contextButton/contextButton.tsx
@@ -44,6 +44,16 @@ function ContextButton(props: Props) {
 					)}
 				</ProductContext.Consumer>
 			);
+			case "removeFromCart":
+			return (
+				<ProductContext.Consumer>
+					{value => (
+						<button onClick={() => value.removeFromCart(props.product)}>
+							removeFromCart
+						</button>
+					)}
+				</ProductContext.Consumer>
+			);
 		default:
 			return null;
 	}

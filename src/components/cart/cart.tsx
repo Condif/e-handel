a9bdb023abcedc1e432c05be7eb8962b9eb10550
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-31 17:10:16 +0200

    added total in cart

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -6,42 +6,71 @@ import {
 	Typography,
 	Grid,
 	createStyles,
-	Theme
+	Theme,
+	Button
 } from "@material-ui/core";
 import ProductFactory from "../productFactory/productFactory";
 import CloseIcon from "@material-ui/icons/Close";
 import { ProductContext } from "../../contexts/productContext";
 import ContextButton from "../contextButton/contextButton";
+import TotalDisplay from "../totalDisplay/totalDisplay"
 
 const useStyles = makeStyles((theme: Theme) =>
-createStyles({
-	list: {
-		marginTop: '4rem',
-		width: "25rem",
-		maxWidth: "100vw",
-		padding: "1rem"
-	},
-	header: {
-		margin: theme.spacing(0,4)
-	},
-	closeIcon: {
-		height: '100%',
-		marginLeft: '1rem',
-		fontSize: 'large'
-	},
-	headerWrapper: {
-		width: '100%',
-		height: '4rem',
-		position: 'fixed',
-		zIndex: 1,
-		backgroundColor: '#FFFFFF',
-		display: 'flex',
-		alignItems: 'center',
-		boxShadow: '0px 5px 5px -2px rgba(0,0,0,0.1)',
-		MozBoxShadow: '0px 5px 5px -2px rgba(0,0,0,0.1)',
-		WebkitBoxShadow: '0px 5px 5px -2px rgba(0,0,0,0.1)',
-	}
-}));
+	createStyles({
+		list: {
+			width: "25rem",
+			maxWidth: "100vw",
+			padding: "1rem"
+		},
+		listWrapper: {
+			position: 'relative',
+			height: 'calc(100% - 11rem)',
+			top: '4rem',
+			width: '100%',
+			overflowY: 'auto'
+		},
+		header: {
+			margin: theme.spacing(0, 4)
+		},
+		closeIcon: {
+			height: '100%',
+			marginLeft: '1rem',
+			fontSize: 'large'
+		},
+		headerWrapper: {
+			width: '100%',
+			height: '4rem',
+			position: 'fixed',
+			zIndex: 1,
+			backgroundColor: '#FFFFFF',
+			display: 'flex',
+			alignItems: 'center',
+			boxShadow: '0px 5px 5px -2px rgba(0,0,0,0.1)',
+			MozBoxShadow: '0px 5px 5px -2px rgba(0,0,0,0.1)',
+			WebkitBoxShadow: '0px 5px 5px -2px rgba(0,0,0,0.1)',
+		},
+		totalWrapper: {
+			width: '100%',
+			height: '7rem',
+			position: 'absolute',
+			bottom: '0',
+			left: '50%',
+			transform: 'translateX(-50%)',
+			zIndex: 1,
+			backgroundColor: '#FFFFFF',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			justifyContent: 'space-evenly',
+			boxShadow: '0px -5px 5px -2px rgba(0,0,0,0.1)',
+			MozBoxShadow: '0px -5px 5px -2px rgba(0,0,0,0.1)',
+			WebkitBoxShadow: '0px -5px 5px -2px rgba(0,0,0,0.1)',
+		},
+		buyButton: {
+			width: '60%',
+			marginBottom: '1rem'
+		}
+	}));
 
 interface Props {
 	isOpen: boolean;
@@ -54,20 +83,32 @@ export default function Cart(props: Props) {
 		<ProductContext.Consumer>
 			{value => (
 				<div className={classes.list} role="presentation">
-					<Grid container>
+					{value.cart.map(item => (
+						<ProductFactory
+							key={item.product.serial}
+							product={item.product}
+							amount={item.amount}
+							productShape="cart"
+						/>
+					))}
+				</div>
+			)}
+		</ProductContext.Consumer>
+	)
 
-
-						<Grid item xs={12}>
-							{value.cart.map(item => (
-								<ProductFactory
-									key={item.product.serial}
-									product={item.product}
-									amount={item.amount}
-									productShape="cart"
-								/>
-							))}
-						</Grid>
-					</Grid>
+	const cartTotal = () => (
+		<ProductContext.Consumer>
+			{value => (
+				<div className={classes.totalWrapper}>
+					<TotalDisplay itemTotal={value.itemTotal} />
+					{(value.itemTotal.itemAmount != 0) 
+					? <Button className={classes.buyButton} variant="contained" color="primary">
+						BUY
+					</Button>
+					: <Button className={classes.buyButton} variant="contained" disabled>
+						add items
+				  	</Button>
+					}
 				</div>
 			)}
 		</ProductContext.Consumer>
@@ -83,7 +124,13 @@ export default function Cart(props: Props) {
 					<Typography className={classes.header} variant="h4">Cart</Typography>
 					<ContextButton shape="clearCart" />
 				</div>
-				{list()}
+				<div className={classes.listWrapper}>
+					{list()}
+
+				</div>
+				<div className={classes.totalWrapper}>
+					{cartTotal()}
+				</div>
 			</Drawer>
 		</div>
 	);

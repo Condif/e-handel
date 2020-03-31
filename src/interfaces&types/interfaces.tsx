4fde51776fdbedd5b36d83aef79e86d3f394af0e
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-03-31 12:00:01 +0200

    added new interface

--- src/interfaces&types/interfaces.tsx
+++ src/interfaces&types/interfaces.tsx
@@ -1,14 +1,20 @@
 import { Sizes } from "./types";
 
 export interface Product {
-    name: string;
-    serial: number;
+	name: string;
+	serial?: number;
 	img: string;
 	price: number;
-    desc: string;
-    
-    info?: {
-        sizes: Sizes[]
-        colors: string[]
-    }
+	desc: string;
+
+	info?: {
+		sizes: Sizes[];
+		colors: string[];
+	};
+}
+export interface NewProduct {
+	name: string;
+	img: string;
+	price: number;
+	desc: string;
 }

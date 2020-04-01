fd0f178279f1d61dd83cde76b08261ebef5c14a1
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-04-01 22:43:32 +0200

    twoOnclick factory

--- src/components/productFactory/productFactory.tsx
+++ src/components/productFactory/productFactory.tsx
@@ -10,17 +10,20 @@ interface Props {
 	productShape: Shape;
 	amount?: number;
 	handleClick?: () => void;
+	alertIsOpen?: boolean;
+	twoOnclickAlert?: () => void;
 }
 
 export default function ProductFactory(props: Props) {
+	
 	switch (props.productShape) {
 		case "card":
-			return <ProductCard product={props.product} handleClick={props.handleClick}/>;
+			return <ProductCard product={props.product}  handleClick={props.handleClick}/>;
 		case "cart":
 			return <ProductCart product={props.product} amount={props.amount}/>;
 		case "listitem":
 			return <div>listItem</div>;
 		case "fullpage":
-			return <ProductPage product={props.product} handleClick={props.handleClick} />;
+			return <ProductPage product={props.product} alertIsOpen={props.alertIsOpen} twoOnclickAlert={props.twoOnclickAlert} />;
 	}
 }

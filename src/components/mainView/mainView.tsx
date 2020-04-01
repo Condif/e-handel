c836f085334a1767861b7fba1e8a96cc5d0c3b20
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-04-01 22:42:40 +0200

    route render

--- src/components/mainView/mainView.tsx
+++ src/components/mainView/mainView.tsx
@@ -7,20 +7,35 @@ import { Product } from "../../interfaces&types/interfaces";
 
 interface Props {
 	handleClick?: () => void;
+	handleClose?: () => void;
+	alertIsOpen: boolean;
 }
 interface State {
 	products: Product[];
 }
+const product: Product[] = [];
 class MainView extends React.Component<Props, State> {
+	
 	render() {
+		console.log('Mainview' + this.props.alertIsOpen);
+		
 		return (
 			<Switch>
+				
 				<Route exact path="/">
 					<ProductGrid handleClick={this.props.handleClick}/>
 				</Route>
 
-				<Route path="/productview/:serial" exact component={ProductView} handleClick={this.props.handleClick} />
-
+				<Route path="/productview/:serial" render={(props) => 
+				<ProductView {...props} 
+				handleClick={this.props.handleClick}
+				alertIsOpen={this.props.alertIsOpen}
+				handleClose={this.props.handleClose}
+				product={product}
+				/>}
+				/>
+					
+				
 				<Route>something went wrong</Route>
 			</Switch>
 		);

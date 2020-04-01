6bfaecdc6dce5246404521e576454524e378128b
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-04-01 11:56:45 +0200

    handle optional

--- src/components/mainView/mainView.tsx
+++ src/components/mainView/mainView.tsx
@@ -6,7 +6,7 @@ import { Switch, Route } from "react-router-dom";
 import { Product } from "../../interfaces&types/interfaces";
 
 interface Props {
-	handleClick: () => void;
+	handleClick?: () => void;
 }
 interface State {
 	products: Product[];

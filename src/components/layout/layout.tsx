9df8a32e364ba6bbbec45df45ce32e20fdd4166d
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-04-02 13:22:33 +0200

    parse fix

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import MainView from "../mainView/mainView";
-import AdminControlls from "../adminControlls/adminControlls";
 import { CssBaseline, Snackbar } from "@material-ui/core";
 import Cart from "../cart/cart";
 import Alert from "@material-ui/lab/Alert";
-import { ProductContext } from "../../contexts/productContext";
-import { AdminContext } from "../../contexts/admin";
 import Topbar from "../topbar/topbar";
 
 function Layout() {
-
-	const [register, setRegister] = React.useState(false)
+	const [register, setRegister] = React.useState(false);
 
 	const setRegisterValue = (value: boolean) => {
-		setRegister(value)	
-	}
+		setRegister(value);
+	};
 	//State for alert
 	const [open, setOpen] = React.useState(false);
 
@@ -23,7 +19,7 @@ function Layout() {
 	};
 
 	const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
-		if (reason === 'clickaway') {
+		if (reason === "clickaway") {
 			return;
 		}
 
@@ -31,47 +27,48 @@ function Layout() {
 	};
 
 	//State for module
-	const [drawer, setDrawer] = React.useState(false)
-
+	const [drawer, setDrawer] = React.useState(false);
 
 	const toggleDrawer = (anchor: string, open: boolean) => {
 		setDrawer(!drawer);
 	};
 
-	const vertical = 'top'
-	const horizontal = 'center'
+	const vertical = "top";
+	const horizontal = "center";
 
 	return (
 		<>
 			<CssBaseline />
 
 			<Topbar isOpen={drawer} toggleDrawer={toggleDrawer} />
-			<MainView />
+			{/* <MainView /> */}
 
-					<Snackbar style={{marginTop: '3rem'}} anchorOrigin={{ vertical, horizontal }} open={open} autoHideDuration={1250} onClose={handleClose}>
-						<Alert style={{minWidth: '15rem'}} color="info" onClose={handleClose} severity="success">
-							Added to the cart
-	        			</Alert>
-					</Snackbar>
-					<MainView 
-						setRegisterValue={setRegisterValue}
-						handleClose={handleClose}
-						handleClick={handleClick}
-					/>
-					<Footer
-						isOpen={drawer}
-						toggleDrawer={toggleDrawer}
-						setRegisterOpen={setRegisterValue}
-						isRegisterOpen={register}
-					/>
-		
-					<Cart
-						isOpen={drawer}
-						toggleDrawer={toggleDrawer}
-						setRegisterOpen={setRegisterValue}
-					/>
-				</>
-			)}
+			<Snackbar
+				style={{ marginTop: "3rem" }}
+				anchorOrigin={{ vertical, horizontal }}
+				open={open}
+				autoHideDuration={1250}
+				onClose={handleClose}>
+				<Alert
+					style={{ minWidth: "15rem" }}
+					color="info"
+					onClose={handleClose}
+					severity="success">
+					Added to the cart
+				</Alert>
+			</Snackbar>
+			<MainView
+				setRegisterValue={setRegisterValue}
+				handleClose={handleClose}
+				handleClick={handleClick}
+			/>
+
+			<Cart
+				isOpen={drawer}
+				toggleDrawer={toggleDrawer}
+				setRegisterOpen={setRegisterValue}
+			/>
+		</>
 	);
 }
 

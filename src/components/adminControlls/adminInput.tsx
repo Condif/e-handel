26a3aab85f4cbc24974ead50bce7ad6b8974f3c5
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-04-02 20:25:05 +0200

    input fix

--- src/components/adminControlls/adminInput.tsx
+++ src/components/adminControlls/adminInput.tsx
@@ -32,38 +32,34 @@ function AdminInput(props: Props) {
 				placeHolder = props.placeHolder?.img;
 				break;
 			case "price":
-				placeHolder = props.placeHolder?.price;
+				placeHolder = props.placeHolder?.price.toString();
 				break;
 		}
 	}
 
 	if (
+		props.mode === "add" &&
 		props.hook.name != undefined &&
 		props.hook.desc != undefined &&
 		props.hook.img != undefined &&
 		props.hook.price != undefined
 	) {
-		props.formHook(true);
+		props?.formHook(true);
 	}
 
 	return (
 		<Grid item sm={12} md={6}>
 			<FormControl fullWidth>
-				{/* <Input
-					id={props.name}
-					onChange={(event: any) =>
-						props.setHook({
-							...props.hook,
-							[`${props.name}`]: event.target.value
-						})
-					}
-					placeholder={placeHolder}
-				/> */}
 				<TextField
 					required
+					type={props.name === "price" ? "number" : "text"}
 					id={props.name}
-					label={"enter " + placeHolder}
-					placeholder={placeHolder}
+					label={
+						props.mode === "edit"
+							? "enter new " + props.name
+							: "enter " + props.name
+					}
+					placeholder={placeHolder ? placeHolder : ""}
 					onChange={(event: any) =>
 						props.setHook({
 							...props.hook,
